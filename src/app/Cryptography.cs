cfcdd85b38a1999ab94425322ad7ad4d63ff3df0
Nick <nicholas.haase@mutualofamerica.com>
2019-02-15 14:25:07 -0500

    Initial

---
+++ src/app/Cryptography.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace pluggable_encryption_console_core_2._2
+{
+    public class Cryptography
+    {
+        public static string Encrypt(string input, SymmetricAlgorithm symmetricAlgorithm, byte[] key, byte[] iv)
+        {
+            // Check arguments.
+            if (string.IsNullOrWhiteSpace(input))
+                throw new ArgumentNullException(nameof(input));
+            if (symmetricAlgorithm == null)
+                throw new ArgumentNullException(nameof(symmetricAlgorithm));
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+            if (iv == null)
+                throw new ArgumentNullException(nameof(iv));
+
+            byte[] encrypted;
+
+            try
+            {
+                symmetricAlgorithm.Key = key;
+                symmetricAlgorithm.IV = iv;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = symmetricAlgorithm.CreateEncryptor(symmetricAlgorithm.Key, symmetricAlgorithm.IV);
+
+                // Create the streams used for encryption.
+                using (var msEncrypt = new MemoryStream())
+                {
+                    using (var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (var swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(input);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            finally
+            {
+                symmetricAlgorithm.Dispose();
+            }
+
+            // Return the encrypted string from the memory stream.
+            return Convert.ToBase64String(encrypted);
+        }
+
+        public static string Decrypt(string cipherText, SymmetricAlgorithm symmetricAlgorithm, byte[] key, byte[] iv)
+        {
+            // Check arguments.
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException(nameof(cipherText));
+            if (symmetricAlgorithm == null)
+                throw new ArgumentNullException(nameof(symmetricAlgorithm));
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+            if (iv == null)
+                throw new ArgumentNullException(nameof(iv));
+
+            // Declare the string used to hold the decrypted text.
+            string plainText = null;
+            var cipherBytes = Convert.FromBase64String(cipherText);
+
+            try
+            {
+                symmetricAlgorithm.Mode = CipherMode.CBC;
+                symmetricAlgorithm.Key = key;
+                symmetricAlgorithm.IV = iv;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = symmetricAlgorithm.CreateDecryptor(symmetricAlgorithm.Key, symmetricAlgorithm.IV);
+
+                // Create the streams used for decryption.
+                using (var ms = new MemoryStream(cipherBytes))
+                {
+                    using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (var sr = new StreamReader(cs))
+                        {
+                            // Read the decrypted bytes from the decrypting stream and place them in a string.
+                            plainText = sr.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            finally
+            {
+                symmetricAlgorithm.Dispose();
+            }
+
+            return plainText;
+        }
+    }
+}
---
+++ src/app/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace pluggable_encryption_console_core_2._2
+{
+    /// <summary>
+    /// This program is an example of making the cryptographic algorithm of choice
+    /// pluggable. This is being done because cryptographic algorithms can frequently be
+    /// made obsolete due to many factors most notably a security vulnerability. By passing
+    /// in the cryptographic algorithm into the encryption and decryption methods, it will
+    /// make swapping out cryptographic algorithms much easier for an application that uses
+    /// them.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Uncomment different lines below to use different
+            // types of symmetric encryption algorithms, but notice
+            // the underlying encryption code doesn't have to change.
+
+            //var symmetricAlgorithm = DES.Create();
+            //var symmetricAlgorithm = TripleDES.Create();
+            var symmetricAlgorithm = Aes.Create();
+
+            // Set key and IV
+            var key = symmetricAlgorithm?.Key;
+            var iv = symmetricAlgorithm?.IV;
+            
+            var original = "This is my password";
+            Console.WriteLine($"Original Value : {original}");
+
+            var encrypted = Cryptography.Encrypt(original, symmetricAlgorithm, key, iv); 
+            Console.WriteLine($"Encrypted Value: {encrypted}");
+
+            var decrypted = Cryptography.Decrypt(encrypted, symmetricAlgorithm, key, iv);
+            Console.WriteLine($"Decrypted Value: {decrypted}");
+
+            Console.ReadKey();
+        }
+    }
+}
